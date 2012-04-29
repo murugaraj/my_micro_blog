5e1fb0ac0ea5c32fdbb75a3d7ea456d23f7c3407
Murugaraj <raj.muruga@gmail.com>
2012-04-29 23:03:23 +0530

    completed micro blog

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,10 +1,18 @@
 class StaticPagesController < ApplicationController
-  def home
-  end
 
+  def home
+    if signed_in?
+      @micropost  = current_user.microposts.build
+      @feed_items = current_user.feed.paginate(page: params[:page])
+    end
+  end
+  
   def help
   end
 
   def about
+  end
+
+  def contact
   end
 end
